6f38d80f34a8e39d613391967087e9a13191a424
Matt Anderson <manderson8787@gmail.com>
2017-06-09 11:40:28 +0800

    Update to 0.19

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -6,19 +6,19 @@ class papertrail::systemd {
     notify  => Service['remote_syslog']
   }
 
-  file { "/tmp/remote-syslog2_0.18_amd64.deb":
+  file { "/tmp/remote-syslog2_0.19_amd64.deb":
     owner   => root,
     group   => root,
     mode    => 644,
     ensure  => present,
-    source  => "puppet:///modules/papertrail/remote-syslog2_0.18_amd64.deb"
+    source  => "puppet:///modules/papertrail/remote-syslog2_0.19_amd64.deb"
   }
 
   package { "remote_syslog":
    provider => dpkg,
-   ensure   => latest,
-   source   => "/tmp/remote-syslog2_0.18_amd64.deb",
-   require     => File['/tmp/remote-syslog2_0.18_amd64.deb'],
+   ensure   => installed,
+   source   => "/tmp/remote-syslog2_0.19_amd64.deb",
+   require     => File['/tmp/remote-syslog2_0.19_amd64.deb'],
   }
 
   service { 'remote_syslog':
