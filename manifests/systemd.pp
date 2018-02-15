698db6061f8f6388084d4c08c4d5f15676fd9be9
Chris Kruger <montdidier@users.noreply.github.com>
2018-02-15 10:05:48 +0800

    fix the remote-syslog2 package name so it does not re-install every 60 seconds (#4)

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -6,25 +6,25 @@ class papertrail::systemd {
     notify  => Service['remote_syslog']
   }
 
-  file { "/tmp/remote-syslog2_0.19_amd64.deb":
+  file { '/tmp/remote-syslog2_0.19_amd64.deb':
     owner   => root,
     group   => root,
     mode    => 644,
     ensure  => present,
-    source  => "puppet:///modules/papertrail/remote-syslog2_0.19_amd64.deb"
+    source  => 'puppet:///modules/papertrail/remote-syslog2_0.19_amd64.deb'
   }
 
-  package { "remote_syslog":
+  package { 'remote-syslog2':
    provider => dpkg,
    ensure   => installed,
-   source   => "/tmp/remote-syslog2_0.19_amd64.deb",
+   source   => '/tmp/remote-syslog2_0.19_amd64.deb',
    require     => File['/tmp/remote-syslog2_0.19_amd64.deb'],
   }
 
   service { 'remote_syslog':
     ensure      => running,
     provider    => 'systemd',
-    require     => Package['remote_syslog'],
+    require     => Package['remote-syslog2'],
   }
 
   service { 'rsyslog':
